671c7b9b70f24f5b87ab49e3864b421c7a9a16fd
lakshankd <dominiclakshank@gmail.com>
2023-08-13 22:25:25 +0530

    adding order-service.

--- order-service/src/main/java/com/lakshankd/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/lakshankd/orderservice/controller/OrderController.java
@@ -1,4 +1,22 @@
 package com.lakshankd.orderservice.controller;
 
+import com.lakshankd.orderservice.dto.OrderRequest;
+import com.lakshankd.orderservice.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/order")
+@RequiredArgsConstructor
 public class OrderController {
+
+    private final OrderService orderService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String placeOrder(@RequestBody OrderRequest orderRequest) {
+        orderService.placeOrder(orderRequest);
+        return "Order placed successfully.";
+    }
 }
