98a202c2abb9f2b0b9b090ce2cd0834ee3113e19
lakshankd <dominiclakshank@gmail.com>
2023-08-15 21:05:45 +0530

    adding connection between order service and inventory service using web client.

--- inventory-service/src/main/java/com/lakshankd/inventoryservice/controller/InventoryController.java
+++ inventory-service/src/main/java/com/lakshankd/inventoryservice/controller/InventoryController.java
@@ -1,9 +1,12 @@
 package com.lakshankd.inventoryservice.controller;
 
+import com.lakshankd.inventoryservice.dto.InventoryResponse;
 import com.lakshankd.inventoryservice.service.InventoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/inventory")
@@ -12,9 +15,11 @@ public class InventoryController {
 
     private final InventoryService inventoryService;
 
-    @GetMapping("/{sku_code}")
+    // http://localhost:8082/api/inventory/iphone_13,iphone_13_red  --> if used Path Variables.
+    // http://localhost:8082/api/inventory?skuCode=iphone_13&skuCode=iphone_13_red --> if used Request Mapping.
+    @GetMapping()
     @ResponseStatus(HttpStatus.OK)
-    public boolean isInStock(@PathVariable("sku_code") String skuCode) {
+    public List<InventoryResponse> isInStock(@RequestParam List<String> skuCode) {
         return inventoryService.isInStock(skuCode);
     }
 }
